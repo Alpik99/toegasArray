6949e9b437841a4d98b00a15756295c84e21a32d
Alpik99 <118905604+Alpik99@users.noreply.github.com>
2023-08-02 16:07:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const waktuBermain = [2,2,3,3,1,4,5];
+const batasWaktu = 2;
+let totalWaktu = 0;
+let melebihiBatas = 0;
+
+for (let i = 0; i < waktuBermain.length; i++) {
+    totalWaktu += waktuBermain[i];
+    if(waktuBermain > batasWaktu){
+        melebihiBatas
+    }
+}
+
+console.log("total waktu bermain adalah " + totalWaktu "jam")
+console.log("jumlah melebihi batas" + melebihiBatas)
